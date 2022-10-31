5c763c9ff2cf53870692e2cd5e115242f48d429f
Sayli Giri <sayligiri555@gmail.com>
2022-10-31 16:24:12 +0530

    cart is auntheticated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import CartS from '../components/CartS';
 import { AddToCart } from '../redux/cart/action';
 
@@ -26,47 +26,54 @@ const Cart = () => {
       navigate("/")
     }
 
+    if(!check){
+      return <Navigate to="/login" replace={true} />
+    }
+    else {
+      return (
+        <div className='cart-main-div'>
+          <div className='cart-products'>
+                <h2>Cart</h2>
+                <div>
+               {
+                cartProducts.map((e)=>
+                <CartS cartpr={e}  />
+                )
+               }
+                 
+                
+                   
+                </div>
+          </div>
+          <div className='cart-total-price-div' >
+             <div className='cart-total-price'>
+             <h2>The Total Amount Of</h2>
+               <div >
+                <h4>Product Amount</h4>
+                <h4>Rs{" "}{TotalPrice}/-</h4>
+               </div>
+               <div >
+                <h4>Shipping Charge</h4>
+                <h4>Rs{" "}500/-</h4>
+               </div>
+               <hr/>
+               <div >
+                <h4>Total Amount</h4>
+                <h4>Rs{" "}{TotalPrice+500}/-</h4>
+               </div>
+               <div>
+                <button onClick={checkout} >Checkout</button>
+                <button onClick={continueShopping}>Continue Shopping</button>
+               </div>
+             </div>
+              
+          </div>
+        </div>
+      )
+    }
 
-  return (
-    <div className='cart-main-div'>
-      <div className='cart-products'>
-            <h2>Cart</h2>
-            <div>
-           {
-            cartProducts.map((e)=>
-            <CartS cartpr={e}  />
-            )
-           }
-             
-            
-               
-            </div>
-      </div>
-      <div className='cart-total-price-div' >
-         <div className='cart-total-price'>
-         <h2>The Total Amount Of</h2>
-           <div >
-            <h4>Product Amount</h4>
-            <h4>Rs{" "}{TotalPrice}/-</h4>
-           </div>
-           <div >
-            <h4>Shipping Charge</h4>
-            <h4>Rs{" "}500/-</h4>
-           </div>
-           <hr/>
-           <div >
-            <h4>Total Amount</h4>
-            <h4>Rs{" "}{TotalPrice+500}/-</h4>
-           </div>
-           <div>
-            <button onClick={checkout} >Checkout</button>
-            <button onClick={continueShopping}>Continue Shopping</button>
-           </div>
-         </div>
-          
-      </div>
-    </div>
-  )
+
+ 
 }
 
 export default Cart
