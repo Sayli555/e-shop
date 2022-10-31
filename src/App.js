bc0527be2a82597411dddadb44a18fd23ab51cfa
Sayli Giri <sayligiri555@gmail.com>
2022-10-31 17:24:24 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Women from './pages/products/Women';
 import Cart from './pages/Cart';
 import Navbar from './components/Navbar';
 import MainRoutes from './pages/MainRoutes';
+import Footer from './components/footer/Footer';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
     <Navbar/>
      <MainRoutes/>
+     <Footer/>
     </div>
   );
 }
